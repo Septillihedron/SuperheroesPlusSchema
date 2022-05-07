4a3e96f0d6cc82ddf0593c0b5de7e8bbf97c606f
Septillihedron <resha3adnandaru@gmail.com>
2022-05-07 08:35:28 +0700

    Seperate preprocessing and compiling

--- compiler/src/index.ts
+++ compiler/src/index.ts
@@ -1,8 +1,10 @@
 import {readFileSync, writeFileSync} from 'fs';
 import JSON5 from 'json5';
+import { compile } from './compiler';
 import { preprocess } from './preprocessor';
 import { Properties } from './Properties';
 
 var code : string = readFileSync('../properties.json', 'utf-8');
-var compiled: Properties = preprocess(JSON5.parse(code));
+var preprocessed: Properties = preprocess(JSON5.parse(code));
+var compiled: object = compile(preprocessed)
 writeFileSync('./compiled-properties.json', JSON.stringify(compiled), 'utf-8');
