8707973545533539d71768fbeb8daa4aa444aa37
Septillihedron <resha3adnandaru@gmail.com>
2022-05-07 08:35:46 +0700

    Implemented preprocessor

--- compiler/src/preprocessor.ts
+++ compiler/src/preprocessor.ts
@@ -1,5 +1,29 @@
-import { Properties } from "./Properties"
+import { Properties, PropertyMap, Type } from "./Properties"
 
-export function preprocess(content: object): Properties {
-	return content as Properties;
+export function preprocess(unprocessed: any): Properties {
+	delete unprocessed.$schema
+	unprocessed = unprocessed as Properties
+	Object.keys(unprocessed).forEach(key => {
+		resolveExtends(unprocessed[key])
+	})
+	return unprocessed
+}
+
+type extendableMap = {[key: string] : Type}
+
+function resolveExtends(extendableMap: extendableMap) : void {
+	Object.keys(extendableMap).forEach(name => {
+		var type = extendableMap[name]
+		var extendsVal = type.extends
+		if (!extendsVal) return
+		delete type.extends
+		var extendedProperties = extendableMap[extendsVal].typeProperties
+		if (!extendedProperties) return
+		var typeProperties = type.typeProperties
+		if (!typeProperties) return
+
+		Object.keys(extendedProperties).forEach(propertyKey => {
+			delete (typeProperties as PropertyMap)[propertyKey]
+		})
+	})
 }
