a4def55e3951409316ebd43e43ce780edc388d18
Septillihedron <resha3adnandaru@gmail.com>
2022-05-08 01:43:14 +0700

    Add addTypes to compiling stage

--- compiler/src/compiler.ts
+++ compiler/src/compiler.ts
@@ -1,5 +1,18 @@
-import * as Preprocessed from "./PreprocessedSchema";
+import * as Preprocessed from "./PreprocessedSchema"
+import * as Compiled from "./CompiledSchema"
 
-export function compile(preprocessed: Preprocessed.Schema): object {
-	return preprocessed;
+export function compile(preprocessed: Preprocessed.Schema): Compiled.Schema {
+	var schema = new Compiled.Schema()
+
+	addTypes(schema, preprocessed)
+
+	return schema
+}
+
+function addTypes({definitions}: Compiled.Schema, preprocessed: Preprocessed.Schema): void {
+	definitions.skill.addSkill("CUSTOM")
+	Object.keys(preprocessed.conditions)
+		.forEach(name => definitions.condition.addType(name))
+	Object.keys(preprocessed.effects)
+		.forEach(name => definitions.effect.addType(name))
 }
