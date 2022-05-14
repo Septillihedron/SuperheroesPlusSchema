0f47f6f1055894e3df073b2d1dceb7676a05014e
Septillihedron <resha3adnandaru@gmail.com>
2022-05-14 14:44:08 +0700

    Add the conditions

--- compiler/src/compiler.ts
+++ compiler/src/compiler.ts
@@ -1,11 +1,16 @@
 import * as Preprocessed from "./PreprocessedSchema"
 import * as Compiled from "./CompiledSchema"
+import { objectPropertyMap } from "./utils"
 
 export function compile(preprocessed: Preprocessed.Schema): Compiled.Schema {
 	var schema = new Compiled.Schema()
 
 	addTypes(schema, preprocessed)
 	addCustomSkill(schema)
+	objectPropertyMap(preprocessed.conditions).forEach((condition: Preprocessed.Condition, name) => {
+		if (condition.available === false) return
+		addCondition(schema, name as string, condition)
+	})
 
 	return schema
 }
@@ -29,4 +34,22 @@ function addCustomSkill({skills}: Compiled.Schema): void {
 		description: "The list of effetcs"
 	})
 	skills["CUSTOM"] = skill
+}
+
+function addCondition({conditions}: Compiled.Schema, name: string, condition: Preprocessed.Condition): void {
+	let compiledCondition = new Compiled.ConditionDefinition(condition.supportedModes!)
+	if (condition.typeProperties !== undefined) {
+		objectPropertyMap(condition.typeProperties)
+			.forEach((property, name) => {
+				let compiledProperty = compileProperty(property, 
+					new Compiled.PropertyClass(conditions, name as string, `#/conditions/${name}`))
+				compiledCondition.addProperty(name as string, compiledProperty, property.required)
+			})
+	}
+	
+	conditions[name] = compiledCondition
+}
+
+function compileProperty(property: Preprocessed.Property, compilingProperty: Compiled.Property): Compiled.Property {
+	return {}
 }
