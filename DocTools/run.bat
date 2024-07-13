ac907d6f4c18b90f55b7bdb7803a8fcba79eb6e8
Septillihedron <resha3adnandaru@gmail.com>
2024-07-13 16:26:38 +0700

    Update minecraft version

--- DocTools/run.bat
+++ DocTools/run.bat
@@ -2,7 +2,7 @@
 
 cd %pluginFolder%\..
 del plugins\DocTools\config.yml
-java -jar Paper.jar
+java -jar paper-1.21-44.jar
 echo F | xcopy /v/y/f plugins\DocTools\config.yml %~dp0\unparsed.yml
 cd %~dp0
 python parse.py
