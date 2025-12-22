9b7b8fcc0be50238388fd350cb254a134a13cdd8
Septillihedron <resha3adnandaru@gmail.com>
2025-12-22 20:02:29 +0700

    Add output directory for translation result

--- schema-translator/src/index.ts
+++ schema-translator/src/index.ts
@@ -1,10 +1,14 @@
-import { readdirSync, rm, writeFileSync } from "fs";
+import { mkdir, mkdirSync, readdirSync, rm, writeFileSync } from "fs";
 import { DocPart } from "./DocPart";
 import { compileToJsonSchema } from "./DocTypeToJsonSchemaCompiler";
 import { compileToDocumentation } from "./DocTypeToDocumentationCompiler";
 import { compileToJsonAISchema } from "./DocTypeToJsonAISchemaCompiler";
 import { compileToJsonAISchema2 } from "./DocTypeToJsonAISchemaCompiler2";
+import { compileToJsonAISchema3 } from "./DocTypeToJsonAISchemaCompiler3";
 import { compileToTsTypes } from "./DocTypeToTsTypes";
+
+const outputDir = "./result";
+mkdirSync(outputDir, { recursive: true })
 
 
 const combinedDocs = readdirSync("../schemaParts")
@@ -12,22 +16,31 @@ const combinedDocs = readdirSync("../schemaParts")
     .filter(path => path.endsWith(".complete.yaoossa"))
     .map(DocPart.loadFile)
     .reduce(DocPart.combine)
-writeFileSync("./combined.yaoossa", combinedDocs.toString())
+writeFileSync(outputDir+"/combined.yaoossa", combinedDocs.toString())
+
 const tsTypes = compileToTsTypes(combinedDocs)
-writeFileSync("./superheroes.yts", tsTypes)
+writeFileSync(outputDir+"/superheroes.yts", tsTypes)
+
 const documentation = compileToDocumentation(combinedDocs)
-rm("./docs", {recursive: true}, (err) => {
+rm(outputDir+"/docs", {recursive: true}, (err) => {
     if (err) console.log(err)
-    documentation.forEach(([file, markdown]) => markdown.save("./docs", file))
+    documentation.forEach(([file, markdown]) => markdown.save(outputDir+"/docs", file))
 })
+
 const lowered = combinedDocs.lowerAll()
-writeFileSync("./lowered.yaoossa", lowered.toString())
+writeFileSync(outputDir+"/lowered.yaoossa", lowered.toString())
+
 const jsonSchema = compileToJsonSchema(lowered)
-writeFileSync("./schema.json", JSON.stringify(jsonSchema, null, 4))
-writeFileSync("../compiler/schemas/Superheroes8+.json", JSON.stringify(jsonSchema)) // compact version
+writeFileSync(outputDir+"/schema.json", JSON.stringify(jsonSchema, null, 4))
+// compact version
+writeFileSync("../compiler/schemas/Superheroes8+.json", JSON.stringify(jsonSchema))
+
 const jsonAISchema = compileToJsonAISchema(lowered)
-writeFileSync("./ai-schema.json", JSON.stringify(jsonAISchema, null, 4))
+writeFileSync(outputDir+"/ai-schema.json", JSON.stringify(jsonAISchema, null, 4))
 writeFileSync("D:\\AI\\geminiAITest\\ai-schema.json", JSON.stringify(jsonAISchema, null, 4))
 const jsonAISchema2 = compileToJsonAISchema2(lowered)
-writeFileSync("./ai-schema2.json", JSON.stringify(jsonAISchema2, null, 4))
+writeFileSync(outputDir+"/ai-schema2.json", JSON.stringify(jsonAISchema2, null, 4))
 writeFileSync("D:\\AI\\geminiAITest\\ai-schema2.json", JSON.stringify(jsonAISchema2, null, 4))
+const jsonAISchema3 = compileToJsonAISchema3(lowered)
+writeFileSync(outputDir+"/ai-schema3.json", JSON.stringify(jsonAISchema3, null, 4))
+writeFileSync("D:\\AI\\geminiAITest\\ai-schema3.json", JSON.stringify(jsonAISchema3, null, 4))
